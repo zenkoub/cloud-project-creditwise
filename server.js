4228bb17bbfc6528890f7ae8c9a5c1e5ee5522e7
zenkoub <koobun1234@gmail.com>
2025-10-30 14:42:41 +0700

    update: timeout server.js

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ import { fileURLToPath } from "url";
 const app = express();
 const PORT = 3000;
 
-// Handle __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve all static files in /public
 app.use(express.static(path.join(__dirname, "public")));
 
-// Serve index.html for all routes (if using SPA navigation)
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () =>
-  console.log(`✅ Server running at http://localhost:${PORT}`)
-);
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+server.keepAliveTimeout = 61000;
+server.headersTimeout = 62000;
